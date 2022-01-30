a9b54d020c58f843b0071d83038bd75bc67929d6
Oscar45ReinosoQ <98630891+Oscar45ReinosoQ@users.noreply.github.com>
2022-01-29 21:06:59 -0500

    interfaces listas

---
+++ app/src/main/java/com/bottomnavigationview/interfaz_productos.java
@@ -0,0 +1,14 @@
+package com.bottomnavigationview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class interfaz_productos extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interfaz_productos);
+    }
+}
---
+++ app/src/main/java/com/bottomnavigationview/interfaz_proveedores.java
@@ -0,0 +1,14 @@
+package com.bottomnavigationview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class interfaz_proveedores extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_interfaz_proveedores);
+    }
+}
